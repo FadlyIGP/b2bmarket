17379ae0f117cbe54994f1b6efc7b279180576c3
FadlyIGP <fadlya179@gmail.com>
2022-06-01 20:47:35 +0700

    privillage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (Auth::user()->role_id==1) {
+           return view('home');
+        }
+        return redirect()->route('home.buyer');
     }
 }
