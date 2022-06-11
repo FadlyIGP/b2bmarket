7238e542661b2129b0320d708fe0d18c8ec6083f
muflihfadly <fadly.sindata@gmail.com>
2022-06-11 10:00:25 +0700

    cms => update delete product;update delete category;listing view item update status transaction - DONE

--- app/Http/Controllers/CmsController/ProductCategoryController.php
+++ app/Http/Controllers/CmsController/ProductCategoryController.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
 
 class ProductCategoryController extends Controller
 {
@@ -41,15 +42,13 @@ class ProductCategoryController extends Controller
         $category = ProdCategory::all();
         foreach ($category as $key => $value) {
             $categorylist[]=[
-                'id'=>$value->id,
-                'name'=>$value->name,
-                'company_name'=>gencompany($value->company_id),
-                'created_at'=>$value->created_at,
+                'id'            => Crypt::encryptString($value->id),
+                'name'          => $value->name,
+                'company_name'  => gencompany($value->company_id),
+                'created_at'    => $value->created_at,
             ];
         }
         return view('productcategory.productcategorylist', ['categorylist' => $categorylist]);
-
-
     }
 
     /**
@@ -71,15 +70,14 @@ class ProductCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        $profile = UserMitra::where('email', Auth::user()->email)->first();
-        $productcategory = new ProdCategory;
-        $productcategory->name = $request->category_name;
-        $productcategory->company_id = $profile->company_id;
+        $profile            = UserMitra::where('email', Auth::user()->email)->first();
+        $productcategory    = new ProdCategory;
+
+        $productcategory->name          = $request->category_name;
+        $productcategory->company_id    = $profile->company_id;
         $productcategory->save();
 
-        return redirect()->route('productcategories.index');
-
-
+        return redirect()->route('productcategories.index')->with('success', 'Successfully Add Data.');
     }
 
     /**
@@ -101,7 +99,15 @@ class ProductCategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $orig_id = Crypt::decryptString($id);
+        $category = ProdCategory::where('id', $orig_id)->first();
+
+        $temp_category = [
+            "id"        => $category->id,
+            "name"      => $category->name
+        ];
+
+        return view('productcategory.editcategory', ['temp_category' => $temp_category]);
     }
 
     /**
@@ -113,7 +119,11 @@ class ProductCategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product_category       = ProdCategory::find($id);
+        $product_category->name = $request->category_name;        
+        $product_category->save();
+
+        return redirect()->route('productcategories.index')->with('success', 'Successfully Update Data.');
     }
 
     /**
@@ -123,7 +133,21 @@ class ProductCategoryController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
-    {
-        //
+    {        
+        $orig_id = Crypt::decryptString($id);
+        $product = MstProduct::where('product_category_id', $orig_id)->first();
+
+        /* PAPUL => If The Category Hava a Product, It Can't Be Deleted */
+        if ($product) {
+           return redirect()->route('productcategories.index')->with('warning', 'The Category Have a Product. Deleting Not Possible !!');
+        }
+
+        $product_category = ProdCategory::where('id', $orig_id)->first();
+        $product_category->delete();
+
+        return redirect()->route('productcategories.index')->with('success', 'Successfully Delete Data.');
     }
+
+
+    /******************** For New Public Function At Here ********************/
 }
--- app/Http/Controllers/CmsController/ProductController.php
+++ app/Http/Controllers/CmsController/ProductController.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
 
 
 class ProductController extends Controller
@@ -35,25 +36,23 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
         $productlist = MstProduct::with('stock', 'image','category')->get();
-
 
         $productlisting = [];
         foreach ($productlist as $key => $value) {
             $productlisting[] = [
-                "id" => $value->id,
-                "product_name" => $value->product_name,
-                "product_descriptions" => $value->product_descriptions,
-                "product_size" => $value->product_size,
-                "product_price" => 'Rp '.''.$value->product_price,
-                "product_item" => $value->product_item,
-                "wishlist_status" => $value->wishlist_status,
-                "company_id" => $value->company_id,
-                "created_at" => $value->created_at,
-                "stock" => $value->stock->qty,
-                "image" => $value->image[0]->img_file,
-                "category"=>$value->category->name
+                "id"                    => Crypt::encryptString($value->id),
+                "product_name"          => $value->product_name,
+                "product_descriptions"  => $value->product_descriptions,
+                "product_size"          => $value->product_size,
+                "product_price"         => 'Rp '.''. number_format($value->product_price),
+                "product_item"          => $value->product_item,
+                "wishlist_status"       => $value->wishlist_status,
+                "company_id"            => $value->company_id,
+                "created_at"            => $value->created_at,
+                "stock"                 => $value->stock->qty,
+                "image"                 => $value->image[0]->img_file,
+                "category"              => $value->category->name
             ];
         }
 
@@ -67,7 +66,6 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
         return view('product.productadd');
     }
 
@@ -83,19 +81,19 @@ class ProductController extends Controller
         $profile = UserMitra::where('email', Auth::user()->email)->first();
 
         $mstprodcuct = new MstProduct;
-        $mstprodcuct->product_name = $request->prod_name;
-        $mstprodcuct->product_descriptions = $request->prod_desc;
-        $mstprodcuct->product_size = $request->prod_size;
-        $mstprodcuct->product_price = $request->prod_price;
-        $mstprodcuct->product_item = $request->prod_item;
-        $mstprodcuct->product_category_id = $request->prod_category;
-        $mstprodcuct->wishlist_status = 0;
-        $mstprodcuct->company_id = $profile->company_id;
+        $mstprodcuct->product_name          = $request->prod_name;
+        $mstprodcuct->product_descriptions  = $request->prod_desc;
+        $mstprodcuct->product_size          = $request->prod_size;
+        $mstprodcuct->product_price         = $request->prod_price;
+        $mstprodcuct->product_item          = $request->prod_item;
+        $mstprodcuct->product_category_id   = $request->prod_category;
+        $mstprodcuct->wishlist_status       = 0;
+        $mstprodcuct->company_id            = $profile->company_id;
         $mstprodcuct->save();
 
         $productstock = new StockProduct;
-        $productstock->product_id = $mstprodcuct->id;
-        $productstock->qty = $request->prod_qty;
+        $productstock->product_id   = $mstprodcuct->id;
+        $productstock->qty          = $request->prod_qty;
         $productstock->save();
 
         if ($request->hasfile('prod_img')) {
@@ -109,12 +107,12 @@ class ProductController extends Controller
 
         foreach ($data as $key => $value) {
             $producimage = new ImgProduct;
-            $producimage->product_id = $mstprodcuct->id;
-            $producimage->img_file = $value;
+            $producimage->product_id    = $mstprodcuct->id;
+            $producimage->img_file      = $value;
             $producimage->save();
         }
 
-        return redirect()->route('products.index');
+        return redirect()->route('products.index')->with('success', 'Successfully Add Data.');
     }
 
     /**
@@ -125,23 +123,23 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
-        $productlist = MstProduct::with('stock', 'image')->where('id', $id)->get();
-        return $productlist;
+        $orig_id = Crypt::decryptString($id);
+        $productlist = MstProduct::with('stock', 'image')->where('id', $orig_id)->get();
+
         $productlisting = [];
         foreach ($productlist as $key => $value) {
             $productlisting[] = [
-                "id" => $value->id,
-                "product_name" => $value->product_name,
-                "product_descriptions" => $value->product_descriptions,
-                "product_size" => $value->product_size,
-                "product_price" => $value->product_price,
-                "product_item" => $value->product_item,
-                "wishlist_status" => $value->wishlist_status,
-                "company_id" => $value->company_id,
-                "created_at" => $value->created_at,
-                "stock" => $value->stock->qty,
-                "image" => $this->urlimg . $value->image[0]->img_file,
+                "id"                    => $value->id,
+                "product_name"          => $value->product_name,
+                "product_descriptions"  => $value->product_descriptions,
+                "product_size"          => $value->product_size,
+                "product_price"         => $value->product_price,
+                "product_item"          => $value->product_item,
+                "wishlist_status"       => $value->wishlist_status,
+                "company_id"            => $value->company_id,
+                "created_at"            => $value->created_at,
+                "stock"                 => $value->stock->qty,
+                "image"                 => $this->urlimg . $value->image[0]->img_file,
             ];
         }
         return view('product.product', ['productlisting' => $productlisting]);
@@ -155,24 +153,33 @@ class ProductController extends Controller
      */
     public function edit($id)
     {
-        //
-        $productlist = MstProduct::with('stock', 'image')->where('id', $id)->first();
+        $orig_id = Crypt::decryptString($id);
+        $productlist = MstProduct::with('stock', 'image', 'category')->where('id', $orig_id)->first();
+
+        $category_id = $productlist->product_category_id;
+        $temp_category = ProdCategory::where('id', $category_id)->first();
+        $category_name = $temp_category->name;
+
+        $category_list = ProdCategory::all();
 
         $productlisting = [
-            "id" => $productlist->id,
-            "product_name" => $productlist->product_name,
-            "product_descriptions" => $productlist->product_descriptions,
-            "product_size" => $productlist->product_size,
-            "product_price" => $productlist->product_price,
-            "product_item" => $productlist->product_item,
-            "wishlist_status" => $productlist->wishlist_status,
-            "company_id" => $productlist->company_id,
-            "created_at" => $productlist->created_at,
-            "stock" => $productlist->stock->qty,
+            "id"                    => $productlist->id,
+            "product_name"          => $productlist->product_name,
+            "product_descriptions"  => $productlist->product_descriptions,
+            "product_size"          => $productlist->product_size,
+            "product_price"         => $productlist->product_price,
+            "product_item"          => $productlist->product_item,
+            "id_category"           => $productlist->product_category_id,
+            "product_category"      => $category_name,
+            "wishlist_status"       => $productlist->wishlist_status,
+            "company_id"            => $productlist->company_id,
+            "created_at"            => $productlist->created_at,
+            "stock"                 => $productlist->stock->qty,
             // "image" => $this->urlimg.$value->image[0]->img_file,
-        ];
+        ];        
 
-        return view('product.productedit', ['productlisting' => $productlisting]);
+        // return dd($productlisting->toJson(200, [], JSON_UNESCAPED_SLASHES));
+        return view('product.productedit', ['productlisting' => $productlisting, 'category_list' => $category_list]);
     }
 
     /**
@@ -184,21 +191,21 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
-        $id = base64_decode($id);
+        // $id = base64_decode($id);
         $mstprodcuct = MstProduct::find($id);
-        $mstprodcuct->product_name = $request->prod_name;
-        $mstprodcuct->product_descriptions = $request->prod_desc;
-        $mstprodcuct->product_size = $request->prod_size;
-        $mstprodcuct->product_price = $request->prod_price;
-        $mstprodcuct->product_item = $request->prod_item;
+        $mstprodcuct->product_name          = $request->prod_name;
+        $mstprodcuct->product_descriptions  = $request->prod_desc;
+        $mstprodcuct->product_size          = $request->prod_size;
+        $mstprodcuct->product_price         = $request->prod_price;
+        $mstprodcuct->product_item          = $request->prod_item;
+        $mstprodcuct->product_category_id   = $request->prod_category;
         $mstprodcuct->save();
 
-        $productstock = StockProduct::where('product_id', $id);
+        $productstock = StockProduct::where('product_id', $id)->first();
         $productstock->qty = $request->prod_qty;
         $productstock->save();
 
-        return redirect()->route('products.index');
+        return redirect()->route('products.index')->with('success', 'Successfully Update Data.');
     }
 
     /**
@@ -209,6 +216,29 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $orig_id = Crypt::decryptString($id);
+        $product = MstProduct::where('id', $orig_id)->first();
+        $product->delete();
+
+        return redirect()->route('products.index')->with('success', 'Successfully Delete Data.');
+    }
+
+
+    /******************** For New Public Function At Here ********************/
+    public function editimage($id)
+    {
+        $orig_id = Crypt::decryptString($id);
+        $temp_image = ImgProduct::where('product_id', $orig_id)->get();
+
+        $image_list = [];    
+        foreach ($temp_image as $key => $value) {
+            $image_list[] = [
+                "id"            => $value->id,
+                "product_id"    => $value->product_id,
+                "image_name"    => $value->img_file
+            ];
+        }
+        
+        return view('product.productimage', ['image_list' => $image_list]);
     }
 }
